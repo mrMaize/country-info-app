d7ff16b36a6b5684d20415d71ea322d409057ce1
kukuruzza <artem.kuka@gmail.com>
2019-10-23 23:28:36 +0500

    Requests base

---
+++ src/utils/api/config.js
@@ -0,0 +1,10 @@
+export const host = {
+    API_HOST: 'https://mfc.aitne.ru/api'
+};
+
+export const requestTypes = {
+    GET: 'GET',
+    POST: 'POST',
+    PUT: 'PUT',
+    DELETE: 'DELETE',
+};
---
+++ src/utils/api/runner.js
@@ -0,0 +1,48 @@
+import {requestTypes} from "./config";
+
+
+function get(type, req) {
+    return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest();
+        request.open(type, req, true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.setRequestHeader('Authorization', `Bearer ${sessionStorage.getItem('token')}`);
+        request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+        request.onload = () => {
+            if (request.status >= 200 && request.status < 300) {
+                if (request.response) {
+                    resolve(JSON.parse(request.response));
+                } else {
+                    resolve();
+                }
+            } else {
+                let response = request.response;
+
+                try {
+                    response = JSON.parse(request.response);
+                } catch (e) {
+                }
+
+                reject(response);
+            }
+        };
+
+        request.onerror = error => {
+            reject(error);
+        };
+
+        request.send();
+    });
+}
+
+export function sendRequest(type, params) {
+    switch (type) {
+        case requestTypes.DELETE:
+        case requestTypes.GET:
+            return get(type, params.url);
+        case requestTypes.PUT:
+        case requestTypes.POST:
+        default:
+            return new Error();
+    }
+}
